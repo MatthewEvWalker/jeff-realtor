77e64c4ead62dd6fedc3870e4465f4ea02cf966b
Matthew Walker <matthewevwalker@gmail.com>
2023-07-08 22:29:17 -0500

    finally fixed navbar and added the cool retail underline hover for the a tags

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 // import Home from './components/Home';
 import Layout from './components/Layout';
+import { useState } from 'react'
 
 
 function App() {
+
+  const [open, setOpen] = useState(false)
+
+
   return (
     <>
       <Routes>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,33 @@
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
-const Navbar = () => (
-    <nav>    
-        <div className='logo-name'>
-            hello
-        </div> 
-        <div className='links'>
-            <ul>
-                <li>
-                    <Link className='linkBtn'to="/"> BUY </Link>
-                </li>
-                <li>
-                    <Link className='linkBtn'to="/">SELL</Link>
-                </li>
-                <li>
-                    <Link className='linkBtn'to="/">ABOUT</Link>
-                </li>
-                <li>
-                    <Link className='linkBtn'to="/">SOCIAL</Link>
-                </li>
-                <li>
-                    <Link className='linkBtn'to="/">CONTACT</Link>
-                </li>
-            </ul>
-        </div> 
-    </nav>
+export default function Navbar() {
 
-)
+    return (
 
-export default Navbar
+        <nav className='nav'>    
+            <a className='logo-name'>
+                WALKER REALTY
+            </a> 
+                <ul>
+                    <li className='active'>
+                        <Link className='link-btn'to="/"> BUY </Link>
+                    </li>
+                    <li>
+                        <Link className='link-btn'to="/">SELL</Link>
+                    </li>
+                    <li>
+                        <Link className='link-btn'to="/">ABOUT</Link>
+                    </li>
+                    <li>
+                        <Link className='link-btn'to="/">SOCIAL</Link>
+                    </li>
+                    <li>
+                        <Link className='link-btn'to="/">CONTACT</Link>
+                    </li>
+                </ul> 
+        </nav>
+
+    )
+}
+
