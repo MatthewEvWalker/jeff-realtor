8fa6a1ef46dd7a84c29029db37b5628853ec2e6b
Matthew Walker <matthewevwalker@gmail.com>
2023-07-13 01:03:17 -0500

    formatted the code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,37 +8,40 @@ import skyline from "../../assets/images/skyline-min.jpeg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
-  faChevronRight
+  faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-    const [currentImage, setCurrentImage] = useState(0)
+  const [currentImage, setCurrentImage] = useState(0);
 
-    const images = [
-        austinNight,
-        lake,
-        austinBridge,
-        skyline
-    ]
+  const images = [austinNight, lake, austinBridge, skyline];
 
-    const handleNextImage = () => {
-        setCurrentImage((currentImage + 1) % images.length)
-    }
+  const handleNextImage = () => {
+    setCurrentImage((currentImage + 1) % images.length);
+  };
 
-    const handlePrevImage = () => {
-        setCurrentImage((currentImage - 1) % images.length)
-    }
+  const handlePrevImage = () => {
+    setCurrentImage((currentImage - 1) % images.length);
+  };
 
-    const handleJumpToImage = (index) => {
-        setCurrentImage(index)
-    }
+  const handleJumpToImage = (index) => {
+    setCurrentImage(index);
+  };
 
   return (
     <>
       <div className="home container">
         <div className="photo-carousel">
-          <FontAwesomeIcon className="icon left" icon={faChevronLeft} onClick={handlePrevImage}/>
-          <FontAwesomeIcon className="icon right" icon={faChevronRight} onClick={handleNextImage}/>
+          <FontAwesomeIcon
+            className="icon left"
+            icon={faChevronLeft}
+            onClick={handlePrevImage}
+          />
+          <FontAwesomeIcon
+            className="icon right"
+            icon={faChevronRight}
+            onClick={handleNextImage}
+          />
 
           <ul>
             <li class="slide" data-active>
@@ -48,16 +51,15 @@ const Home = () => {
         </div>
 
         <div className="carousel-nav">
-            {images.map((image, index) => (
-                <button key={index} className={`carousel-circle ${index === currentImage ? 'carousel-circle.active-slide' : ''}`} onClick={() => handleJumpToImage(index)}>
-
-
-                </button>
-                
-            ))}
-
-
-                      
+          {images.map((image, index) => (
+            <button
+              key={index}
+              className={`carousel-circle ${
+                index === currentImage ? "carousel-circle.active-slide" : ""
+              }`}
+              onClick={() => handleJumpToImage(index)}
+            ></button>
+          ))}
         </div>
       </div>
     </>
