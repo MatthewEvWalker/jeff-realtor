6cac6163a904049bb13cf39d5ab336d241db01b3
Matthew Walker <matthewevwalker@gmail.com>
2023-07-13 01:02:29 -0500

    changed the layout of the photo carousel, it works but it needs to have a set active class that works. I am not sure why it isn't working.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,42 +1,63 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import "./Home.scss";
-import bridge from "../../assets/images/bridge.jpg";
+import lake from "../../assets/images/lake.jpg";
 import austinNight from "../../assets/images/nighttime-austin.jpg";
-import jenga from "../../assets/images/skyline-min.jpeg";
+import austinBridge from "../../assets/images/landscape-austin-house.jpeg";
+import skyline from "../../assets/images/skyline-min.jpeg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronLeft,
-  faChevronRight,
-  faCircle,
+  faChevronRight
 } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
+    const [currentImage, setCurrentImage] = useState(0)
+
+    const images = [
+        austinNight,
+        lake,
+        austinBridge,
+        skyline
+    ]
+
+    const handleNextImage = () => {
+        setCurrentImage((currentImage + 1) % images.length)
+    }
+
+    const handlePrevImage = () => {
+        setCurrentImage((currentImage - 1) % images.length)
+    }
+
+    const handleJumpToImage = (index) => {
+        setCurrentImage(index)
+    }
+
   return (
     <>
       <div className="home container">
         <div className="photo-carousel">
-          <FontAwesomeIcon className="icon left" icon={faChevronLeft} />
-          <FontAwesomeIcon className="icon right" icon={faChevronRight} />
+          <FontAwesomeIcon className="icon left" icon={faChevronLeft} onClick={handlePrevImage}/>
+          <FontAwesomeIcon className="icon right" icon={faChevronRight} onClick={handleNextImage}/>
 
           <ul>
             <li class="slide" data-active>
-              <img src={austinNight} alt="photo 1" />
-            </li>
-
-            <li class="slide">
-              <img src={jenga} alt="photo 2" />
-            </li>
-
-            <li class="slide">
-              <img src={bridge} alt="photo 3" />
+              <img src={images[currentImage]} alt="" />
             </li>
           </ul>
         </div>
 
         <div className="carousel-nav">
-            <button className="carousel-circle"></button>
-            <button className="carousel-circle"></button>
-            <button className="carousel-circle"></button>            
+            {images.map((image, index) => (
+                <button key={index} className={`carousel-circle ${index === currentImage ? 'carousel-circle.active-slide' : ''}`} onClick={() => handleJumpToImage(index)}>
+
+
+                </button>
+                
+            ))}
+
+
+                      
         </div>
       </div>
     </>
